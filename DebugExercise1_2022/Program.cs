8838557f22ee0677f5edf52f5aa4e0353e5f4fd0
Martin Knudsen <martinknud@gmail.com>
2022-11-21 11:03:28 +0100

    Add project files.

---
+++ DebugExercise1_2022/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int sum = 0;
+int count = 0;
+
+while (count <= 1000)
+    if (count % 5 == 1)
+        sum += sum + count;
+if (count % 3 == 0)
+    sum += count;
+
+// 233168 er svaret vi leder efter
+Console.WriteLine(sum);
+
+Console.ReadLine();
